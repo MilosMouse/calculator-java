2261f3b3b631ac7ba6f0f6ec838772a2e619a150
MilosMouse <126770896+MilosMouse@users.noreply.github.com>
2023-12-10 12:01:43 -0500

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,15 +1,32 @@
-import unittest
-from Calculator import Calculator
+import org.junit.Assert;
+import org.junit.Test;
 
-class SimpleTest(unittest.TestCase);
-    def test_1(self);
-        ADDITION_SYMBOL = "+";
-        SUBTRACTION_SYMBOL = "-";
-        MULTIPLICATION_SYMBOL = "*";
-        DIVISION_SYMBOL = "/";
-        result = Calculator.calculate_ideal_weight(ADDITION_SYMBOL, SUBTRACTION_SYMBOL);
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
-        self.assertEqual(MULTIPLICATION_SYMBOL, DIVISION_SYMBOL, result);
 
-if __name__=="__main__";
-    unittest.main();
+public class CalculatorTest {
+
+
+    @Test
+    public void mnozenje(){
+
+        List<Float> numbers = new ArrayList<Float>((Collection<Float>) Arrays.asList(5.0f, 3.0f));
+        List<String>operations=new ArrayList<>(Arrays.asList("*"));
+
+        Calculator.Calculate(numbers,operations);
+
+        float expected=15.0f;
+        float actual=Calculator.finalResult;
+        Assert.assertEquals(expected,actual,0.0f);
+
+
+
+
+
+
+    }
+
+}
